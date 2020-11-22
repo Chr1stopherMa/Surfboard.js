c6942da1d06ff9585162b1929eb46782de79aa92
Christopher Ma <chrisma200@gmail.com>
2020-11-22 01:02:49 -0500

    Add a few more functions

--- Surfboard.js
+++ Surfboard.js
@@ -31,29 +31,47 @@ function Surfboard() {
         /* Expands element by a specified factor.
          *
          * Options:
-         *      animation: Time to expand; default 1 second.
+         *      delay: Time to expand; default 1 second.
          *          Set to false to turn off.
          *      expandX: Multipler to width; default 2
          *      expandY: Multipler to height; default 2
          *      toggle: If set to true, expanding/shrinking
          *          is only triggered by specified key event
          *          (and not the absence of the event).
+         * 
+         * TODO: image does not scale properly when height/width
+         *       is defined using percentage.
         */
+
         element = _get_element(element);
-        const domRect = element.getBoundingClientRect();
-        const height = domRect.height;
-        const width = domRect.width;
+        options = options ? options : {}
+        const domRect = () => {return element.getBoundingClientRect()};
+        let height, width;
+
+        function updateSize() {
+            height = domRect().height;
+            width = domRect().width;
+            element.style.height = `${height}px`;
+            element.style.width = `${width}px`;  
+        }
+
+        if (element.complete)
+            updateSize();
+        else
+            element.onload = updateSize; // In case element has not been loaded
 
         // set animation
-        if (!("animation" in options)) {
+        if (!("delay" in options)) {
             element.style.transition = 'width 1s, height 1s';
-        } else if (options.animation) {
-            const time = options.animation;
+        } else if (options.delay) {
+            const time = options.delay;
             element.style.transition = `width ${time}s, height ${time}s`;
         }
 
-        const expandX = "expandX" in options ? options.expandX : 2;
-        const expandY = "expandY" in options ? options.expandY : 2;
+        options.expand = options.expand ? options.expand : 1;
+
+        const expandX = "expandX" in options ? options.expandX : options.expand;
+        const expandY = "expandY" in options ? options.expandY : options.expand;
 
 
         function active() {
@@ -63,15 +81,14 @@ function Surfboard() {
 
         function unactive() {
             element.style.height = `${height}px`;
-            element.style.width = `${width}px`;      
+            element.style.width = `${width}px`;  
         }
 
         if ('toggle' in options && options.toggle) {
             keys.map((key) => key.hold = false);
-            _manager.addEvent(keys, _make_toggle(active, unactive));
-        }
-        else
-            _manager.addEvent(keys, active, unactive);
+            setTimeout(() => _manager.addEvent(keys, _make_toggle(active, unactive)), 400);
+        } else
+            setTimeout(() =>_manager.addEvent(keys, active, unactive), 400);
     }
 
 
@@ -88,7 +105,7 @@ function Surfboard() {
         */
 
        element = _get_element(element);
-
+       options = options ? options : {}
        let delay = 0, timeout, _default;
 
        if ('delay' in options) {
@@ -125,6 +142,52 @@ function Surfboard() {
     }
 
 
+    function slide(keys, element, options) {
+        /* Slides the element.
+         *
+         * Options:
+         *      horizontal: The amount of horizontal scroll; default is 0.
+         *          If this value is an integer, it represents the number
+         *          of pixels. If this value is a float, it represents the
+         *          percentage of the viewport.
+         *      relative: If true, viewport is scrolled relative to the 
+         *          current position. Otherwise, the viewport is scrolled
+         *          to the absolute position.
+         *      smooth: If true, sets scroll-behavior of <html> tag to smooth.
+         *      vertical: The amount of vertical scroll; default is 0.
+         *          If this value is an integer, it represents the number
+         *          of pixels. If this value is a float, it represents the
+         *          percentage of the viewport.  
+        */
+
+        options = options ? options : {};
+        element = _get_element(element);
+        const domRect = () => {return element.getBoundingClientRect()};
+
+        element.style.width = `${domRect().width}px`;
+
+        if (options.duration)
+            element.style.transition += `width ${options.duration}s`;
+        else
+            element.style.transition += `width 1s`;
+
+        const original = element.style.width;
+        element.style.overflow = 'hidden';
+        element.style.position = 'absolute';
+        element.style.animation = '2s slide-right 2s forwards';
+        
+        function active() {
+            element.style.width = '0px';
+        }
+
+        function inactive() {
+            element.style.width = original;
+        }
+
+        _manager.addEvent(keys, active, inactive);
+    }
+
+
     function scroll(keys, options) {
         /* Moves the screen to a specified location.
          *
@@ -137,15 +200,13 @@ function Surfboard() {
          *          current position. Otherwise, the viewport is scrolled
          *          to the absolute position.
          *      smooth: If true, sets scroll-behavior of <html> tag to smooth.
-         *          WARNING: this may have unintentional side-effects.
          *      vertical: The amount of vertical scroll; default is 0.
          *          If this value is an integer, it represents the number
          *          of pixels. If this value is a float, it represents the
          *          percentage of the viewport.  
         */
 
-        if (options.smooth)
-            document.documentElement.style.scrollBehavior = 'smooth';
+        options = options ? options : {};
 
         let x_scroll, y_scroll;
         options.horizontal = options.horizontal ? options.horizontal : 0;
@@ -162,13 +223,35 @@ function Surfboard() {
             y_scroll = function() {return options.vertical * window.innerHeight};
 
         function active() {
+            if (options.smooth)
+                document.documentElement.style.scrollBehavior = 'smooth';
             if (options.relative)
                 scrollBy(x_scroll(), y_scroll());
             else
                 scrollTo(x_scroll(), y_scroll());
+            if (options.smooth)
+                document.documentElement.style.scrollBehavior = 'initial';
         }
 
         _manager.addEvent(keys, active);
+    }
+
+
+    function custom(keys, callback, options, callback2) {
+        /* Define a custom event.
+         *
+         * Options:
+         *      toggle: If true and callback2 is defined,
+         *          key events will alternate between
+         *          callback and callback2.
+        */   
+
+       options = options ? options : {};
+
+       if (options.toggle)
+           _manager.addEvent(keys, _make_toggle(callback, callback2));
+        else
+           _manager.addEvent(keys, callback, callback2);
     }
 
 
@@ -176,6 +259,8 @@ function Surfboard() {
     return {
         expand: expand,
         pop: pop,
-        scroll: scroll
+        slide: slide,
+        scroll: scroll,
+        custom: custom
     }
 }
